55e7fb0fa9c14024788d7a725b5ae3db12d64502
Levomäki Aapo O <levomaaa@cubbli22-gold.local>
2024-04-09 17:39:41 +0300

    Users can be created to database

---
+++ src/entities/user.py
@@ -0,0 +1,18 @@
+class User:
+    """Luokka kuvaa yksittäistä käyttäjää.
+
+    Attributes:
+        username: Käyttäjätunnus merkkijonoarvona.
+        password: Salasana merkkijonoarvona.
+    """
+
+    def __init__(self, username, password):
+        """Luokan konstruktori, joka luo uuden käyttäjän.
+
+        Args:
+            username: Käyttäjätunnus merkkijonoarvona.
+            password: Salasana merkkijonoarvona.
+        """
+
+        self.username = username
+        self.password = password
---
+++ src/repositories/user_repository.py
@@ -0,0 +1,64 @@
+from entities.user import User
+from database_connection import get_database_connection
+
+
+def get_user_by_row(row):
+    return User(row["username"], row["password"]) if row else None
+
+
+class UserRepository:
+    """Luokka vastaa käyttäjiin liittyvistä tietokantaoperaatioista.
+    """
+
+    def __init__(self, connection):
+        """Luokan konstruktori.
+
+        Args:
+            connection: Tietokantayhteyden connection-olio
+        """
+
+        self._connection = connection
+
+    def create(self, user):
+        """Luo käyttäjän tietokantaan.
+        """
+
+        cursor = self._connection.cursor()
+
+        cursor.execute(
+            "insert into users (username, password) values (?, ?)",
+            (user.username, user.password)
+        )
+
+        self._connection.commit()
+
+        return user
+    
+    def delete_all(self):
+        """Poistaa kaikki käyttäjät tietokannasta.
+        """
+
+        cursor = self._connection.cursor()
+
+        cursor.execute("delete from users")
+
+        self._connection.commit()
+
+    def find_all(self):
+        """Palauttaa kaikki käyttäjät listana.
+
+        Returns:
+            Palauttaa listan kaikista User-olioista.
+        """
+
+        cursor = self._connection.cursor()
+
+        cursor.execute("select * from users")
+
+        rows = cursor.fetchall()
+
+        return list(map(get_user_by_row, rows))
+
+
+
+user_repository = UserRepository(get_database_connection())
